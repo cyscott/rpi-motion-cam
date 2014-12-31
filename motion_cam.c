f37f3ddc01f9204200892a2cec7918f02b5e7ccf
Cy Scott <cscott@gmail.com>
2014-12-31 09:34:41 -0800

    Initial Commit

---
+++ motion_cam.c
@@ -0,0 +1,28 @@
+#include <bcm2835.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PIN RPI_GPIO_P1_08
+
+int main(int argc, char **argv)
+{
+  if(!bcm2835_init()) {
+    printf("Oh shit. Library bcm2835 not loaded.");
+    return 1;
+  }
+
+  bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_INPT);
+  printf("Okay, here we go.\n");
+
+  while(1)
+  {
+    if(bcm2835_gpio_lev(PIN))
+    {
+      //system("raspistill -q 5 -o pic.jpg -t 1 -th 0:0:0");
+      printf("Snap!\n");
+      while(bcm2835_gpio_lev(PIN));
+    }
+    bcm2835_delay(10);
+  }
+
+}
